7052a30810e63881a7f3286e8e1445c6ca43f04e
mateuszszmytko <mateusz.szmytko@gmail.com>
2019-04-04 19:46:19 +0200

    babel polyfills include

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -39,7 +39,14 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                  "useBuiltIns": "usage"
+                                }
+                            ]
+                        ],
                     }
                 }
             }
